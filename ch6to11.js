ceeeb59fcdac9fc2c15019f007ffd3cc82d2d13d
SahirMalkani <41510686+SahirMalkani@users.noreply.github.com>
2023-09-11 19:28:30 +0500

    Add files via upload

---
+++ ch6to11.js
@@ -0,0 +1,340 @@
+// chapter 6
+
+//1.
+
+
+var x=1;
+
+x++
+
+//alert(x);
+
+//or 
+
+++x
+
+
+//2.
+
+var x=100;
+
+
+--x //most fastest
+alert(x);
+
+//or
+x--
+
+//alert(x);
+
+
+
+//3.
+
+var x = 50;
+var y = x++;
+
+alert(y); //its 50
+
+
+//4.
+
+
+var y = 50;
+var z = --y;
+
+alert(z); 
+
+
+//5.
+
+
+var zz=100;
+
+var aa;
+
+aa=zz--;
+
+
+//6.
+
+
+var zz1=100;
+
+var aa1;
+
+aa1=zz1++;
+
+//7.
+
+
+var zz=100;
+
+var aa;
+
+aa=++zz;
+
+alert(aa);
+
+
+//chapter 7
+
+//1.
+
+var calculatedNum = 2 + (2 * 6);
+
+//its 14
+
+
+//2.
+
+var calculatedNum = (2 + 2) * 6;
+
+//its 24
+
+//3.
+var calculatedNum = (2 + 2) * (4 + 2);
+
+//its  24
+
+//4.
+var calculatedNum = ((2 + 2) * 4) + 2;
+
+
+// its 18
+
+
+
+//5.
+
+ (2 + 2) * (4 + 10);
+
+
+ //6.
+
+var bb;
+ bb=4 / (2 * 4);
+
+
+ alert(bb);
+
+ //Chapter 8
+
+
+ //1.
+ var num = "2" + "2";
+
+ //its 22
+
+
+
+ //2.
+ message = ("Hello," + "Dolly");
+
+alert(message);
+
+
+
+//3.
+
+
+var num1=33;
+
+alert(num1 + "+"+ "3");
+
+
+//5.
+
+var vcname="Pakistan";
+
+var vzname= "Zindabad";
+
+
+alert (vcname+" "+vzname);
+
+
+//6.
+
+
+
+var vcname="Pakistan";
+
+var vzname= "Zindabad";
+
+var v3var =vcname+" "+vzname
+
+
+alert (v3var);
+
+
+//chapter 9
+
+//1.
+
+
+var myFirstname=prompt("Enter first name");
+
+
+
+//2.
+
+var vcountry =prompt("country name","China");
+
+
+//3.
+
+
+var yourName = prompt("Enter Your Name");
+
+//4.
+
+var vmsg =prompt("Welcome Screen","Hello friends");
+
+
+
+//5.
+
+var m1 ="welcome box";
+
+var m2="hello friends";
+
+
+var m3= prompt(m1,m2);
+
+//6.
+
+var m1 ="last q of prompt";
+
+var m2="6th question";
+
+var m3= prompt(m1,m2);
+
+alert(m3);
+
+
+//chapter 10
+
+//1.
+
+
+var city = "Karachi";
+if (city = "Karachi") {
+console.log("The City OF Lights")
+
+}
+
+
+
+//2.
+
+
+
+if (x === y) {
+
+    var xx=prompt("whats the value of z",50)
+
+
+}
+
+
+//3.
+
+
+var xx=+prompt("enter zip code of your city")
+
+
+if (xx === 10010) {
+
+    alert("Karachi")
+
+}
+
+else{
+
+
+    alert("enter correct and")
+}
+
+
+
+//4.
+
+
+
+if (xx >0) {
+
+    xx=100;
+
+}
+
+else{
+
+
+    alert("not the real number")
+
+
+
+}
+
+
+
+//chapter 11
+
+
+var xx=10;
+var yy=30;
+
+
+if (xx !==yy) {
+
+    
+
+}
+
+
+//2.
+
+if (xx >=yy) {
+
+    
+
+}
+
+
+//3.
+
+if (xx !==50) {
+
+    xx=60;
+
+
+}
+
+
+
+//4.
+if (60 !==50) {
+
+alert("conrtaulations");
+    
+
+}
+
+
+
+//5.
+
+var firstName=prompt("enater first name");
+
+
+if (firstName !=="Sahir") {
+
+    alert("No match found");
+        
+    
+    }
+    
+    
+
+
+
