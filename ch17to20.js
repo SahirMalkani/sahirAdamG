2169dda7c5ef190db3a06f1a0ca71667a675608e
SahirMalkani <41510686+SahirMalkani@users.noreply.github.com>
2023-09-19 02:14:37 -0700

    Add files via upload

---
+++ ch17to20.js
@@ -0,0 +1,162 @@
+
+// q`1
+
+for (let i = 0; i < 10; i++) {
+    vsahir1="number# is"+ i +"<br>";
+
+console.log(vsahir1);
+
+} 
+
+
+//q2
+
+
+  for (let i = 0; i < 12; i++) {
+   vsahir= "# is"+ i +"<br>";
+
+
+
+console.log(vsahir);
+
+} 
+
+
+// q3
+
+for (var i = 0;i <= 4; i++) {
+
+}
+
+//q4
+
+
+for (var vPet = 0;vPet < 100; vPet++) {
+
+
+console.log(vPet);
+
+
+}
+
+
+//q5
+
+
+for (var vPet1 = 1000;vPet1 > 997; vPet1--) {
+
+
+    console.log(vPet1);
+    
+}    
+    
+//q6
+
+
+
+//q7
+
+var flag=true;
+
+
+//q8
+
+const Pets = ["cat","dog","puppy","kitten"];
+
+
+for (let i = 0; i < Pets.length; i++) {
+  
+
+
+}
+
+
+//q9
+
+
+for (let i = 0; i < 10; i++) {
+
+    
+console.log(i);
+
+
+
+if(i===1){
+
+    console.log(i+"  its 2nd iteration");
+
+break;
+
+
+}
+
+
+} 
+
+//q10
+
+
+const UserNames = ["sahir","kashif","nomir","tehseen"];
+
+
+var userName=prompt("enter user name");
+
+console.log(UserNames);
+   
+
+for (let i = 0; i < UserNames.length; i++) {
+
+    
+    
+    
+    
+    if(UserNames[i]===userName){
+    
+        alert("matched at index#" +i );
+
+    }else{
+
+    alert("enter correct name");
+    
+    }
+    
+    
+    } 
+
+    //q 11
+
+    const list = ["sahir","kashif","nomir","tehseen"];
+
+    var userInput=prompt("enter user name");
+
+
+    for (var i = 0; i < list.length; i++) {
+        if (userInput === list[i]) {
+        alert("Match found");
+        matchFound = true; break;
+
+        }else{
+
+        alert("enter correct name");
+        
+        }
+        
+
+        };
+
+
+//q12
+
+var firstArr = ["a","b","c","d","e","f"];
+
+var secondArr = [1, 2, 3, 4, 5, 6];
+
+
+for (var i = 1; i = firstArr.length; i++) {
+
+
+}
+    
+
+
+
