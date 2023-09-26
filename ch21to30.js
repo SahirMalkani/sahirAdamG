e1a2576ddb83d2a31a8ed3c9fc65b8d36f5351fd
SahirMalkani <41510686+SahirMalkani@users.noreply.github.com>
2023-09-26 18:19:16 +0500

    Add files via upload

---
+++ ch21to30.js
@@ -0,0 +1,63 @@
+//q1
+ 
+var userInput=prompt("sdf");
+
+var allLower = userInput.toLowerCase();
+
+alert (allLower);
+
+//Q2
+
+var X = "SAHIR";
+
+var X = X.toLowerCase();
+
+
+
+alert (X);
+
+//Q3
+
+
+var Y = "sahir";
+
+var Y = Y.toUpperCase();
+
+
+
+alert (Y);
+
+
+//Q4
+
+
+var Y = "SAHIR";
+
+var Z = Y.toLowerCase();
+
+
+
+alert (Z);
+
+
+//Q6
+
+
+var Y = "sahir guest HOUSE";
+
+var Y = Y.toUpperCase();
+
+
+
+alert (Y);
+
+//Q7
+
+
+var cityName = "kaRacHi";
+
+var fch=cityName.slice(0,1).toUpperCase()
+var rch=cityName.slice(1).toLowerCase()
+
+
+alert(fch+rch);
