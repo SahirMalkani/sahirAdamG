cc80aa10297d02f9648310674d4e129f8a5e9413
SahirMalkani <41510686+SahirMalkani@users.noreply.github.com>
2023-11-07 21:07:28 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,69 @@
-//chapter 3
 
-var vAge = "15";
 
-alert("i am" + vAge + " years old");
+var firebaseConfig = {
+  apiKey: "AIzaSyAo0iR8przglSEej7r78FDaiLzxFdvZNWY",
+  authDomain: "sahirauth-b11f1.firebaseapp.com",
+  projectId: "sahirauth-b11f1",
+  storageBucket: "sahirauth-b11f1.appspot.com",
+  messagingSenderId: "754045400356",
+  appId: "1:754045400356:web:7cc9ea4df12ab6d9a9efb1"
+};
 
-var N = "14";
+// Initialize Firebase
+var app = initializeApp(firebaseConfig);
 
-alert("you have visited" + N + " times the page");
 
-var birthYear;
 
-birthYear = 1993;
+function clk(){
 
-document.write("my birth year is " + birthYear);
+var inp=document.getElementById("t1")
+var getdt =localStorage.getItem("name1")
 
-vtype = typeof birthYear;
+var arr=JSON.parse(getdt)
 
-document.write("data type of my dec variable is \n" + vtype);
+if(!arr){
+arr=[]
 
-//Q4
 
-var vWeb = "xyzclothingstore.com";
+}
 
-var visitorName = "John Doe";
 
-var ProdName = "Nike T sihrts";
+var obj ={
+    name:inp.value,
+age:21
 
-var vQty = 24;
 
-document.write(
-  "<br>" + visitorName + " ordered " + vQty + " " + ProdName + " on " + vWeb
-);
+    
+}
+
+
+arr.push(obj)
+
+localStorage.setItem("name1",JSON.stringify(arr))
+
+localStorage.setItem("name2",JSON.stringify(arr))
+
+}
+
+
+
+function signup(){
+
+  var uu1 =document.getElementById("u1").value
+  var pp1 =document.getElementById("p1").value
+
+
+  firebase.auth().createUserWithEmailAndPassword(uu1, pp1)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    alert (user);
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert (errorMessage);
+  });
+
+  
+}
